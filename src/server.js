01c33fe37c680e4d699447ceb6a00d5057bbf4e5
Seokyoung Han <hs04111@naver.com>
2021-04-28 20:21:35 +0900

    eslint installed

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
-import express from "express";
+import express from 'express';
 
-const PORT = 4000
+const PORT = 4000;
 
 const app = express();
 
-const handleHome = () => console.log("Someone is trying to go home")
+const handleHome = (req, res) => {
+    console.log(req.method, req.url);
+    return res.send('Hello World!');
+};
 
-app.get("/", handleHome)
+const handleLogin = (req, res) => {
+    return res.send('Login Here!');
+};
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`)
+app.get('/', handleHome);
+app.get('/login', handleLogin);
 
-app.listen(PORT, handleListening)
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
