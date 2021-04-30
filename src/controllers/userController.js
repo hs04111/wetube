8265fe7cf67f9ae2689202714fe92ddd5a897980
Seokyoung Han <hs04111@naver.com>
2021-04-30 14:45:40 +0900

    regular expression applied on :id

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,9 @@
 export const join = (req, res) => res.send('Join');
 export const login = (req, res) => res.send('Login Here!');
 export const edit = (req, res) => res.send('Edit Here!');
-export const see = (req, res) => res.send('See Users');
+export const see = (req, res) => {
+    console.log(req.params.id);
+    res.send('See Users');
+};
 export const logout = (req, res) => res.send('Log Out');
 export const remove = (req, res) => res.send('Delete User');
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,9 +3,9 @@ import { see, edit, logout, remove } from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get('/:id', see);
-userRouter.get('/logout', logout);
+userRouter.get('/:id(\\d+)', see);
 userRouter.get('/edit', edit);
+userRouter.get('/logout', logout);
 userRouter.get('/remove', remove);
 
 export default userRouter;
