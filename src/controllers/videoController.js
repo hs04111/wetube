48d9984b5fc40358779622217ae59415bbdf4289
Seokyoung Han <hs04111@naver.com>
2021-05-07 11:22:58 +0900

    mixin & array database practice part 1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,29 @@
-const fakeUser = {
-    username: 'Ong',
-    loggedIn: false
-};
-const videos = [1, 2, 3, 4, 5];
+const videos = [
+    {
+        title: 'First Video',
+        views: 13,
+        id: 1
+    },
+    {
+        title: 'Second Video',
+        views: 13,
+        id: 2
+    },
+    {
+        title: 'Third Video',
+        views: 13,
+        id: 3
+    }
+];
 
 export const trending = (req, res) =>
-    res.render('home', { pageTitle: 'Home!', fakeUser, videos });
-export const see = (req, res) =>
-    res.render('watch', { pageTitle: 'Watch Video!' });
+    res.render('home', { pageTitle: 'Home!', videos });
+export const see = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    res.render('watch', { pageTitle: `Watch ${video.title}!`, video });
+};
+
 export const search = (req, res) => res.send('Search Videos');
 export const edit = (req, res) => res.send('Edit Videos');
 export const deleteVideo = (req, res) => res.send('Delete Video');
