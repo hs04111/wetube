ad023d853580b510cca4d27e4de0f481a55b941e
Seokyoung Han <hs04111@naver.com>
2021-05-14 13:18:21 +0900

    duplication check with  operator  & password confirmation

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,16 @@ import User from '../models/User';
 
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
 export const postJoin = async (req, res) => {
-    const { name, email, password, location, username } = req.body;
+    const { name, email, password, password2, location, username } = req.body;
+    const exists = await User.exists({ $or: [{ username }, { email }] });
+    if (password !== password2) {
+        const errMessage = 'Password confirmation does not match';
+        return res.render('join', { pageTitle: 'Join', errMessage });
+    }
+    if (exists) {
+        const errMessage = 'The username/email already exists';
+        return res.render('join', { pageTitle: 'Join', errMessage });
+    }
     await User.create({
         name,
         email,
