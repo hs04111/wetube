02a4d588424f481fec08f923d79f4ba235ced8f4
Seokyoung Han <hs04111@naver.com>
2021-05-14 11:54:25 +0900

    Create Accout & password hashing

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,18 @@
+import User from '../models/User';
+
 export const getJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
-export const postJoin = (req, res) => res.render('join', { pageTitle: 'Join' });
+export const postJoin = async (req, res) => {
+    const { name, email, password, location, username } = req.body;
+    await User.create({
+        name,
+        email,
+        password,
+        location,
+        username
+    });
+
+    res.redirect('/login');
+};
 export const login = (req, res) => res.send('Login Here!');
 export const edit = (req, res) => res.send('Edit Here!');
 export const see = (req, res) => {
