f96ea31aedad9fc568a589dcec1f409c0dfd95df
Seokyoung Han <hs04111@naver.com>
2021-05-20 11:11:30 +0900

    creating user from social login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -116,9 +116,28 @@ export const finishGithubLogin = async (req, res) => {
                 }
             })
         ).json();
-        const email = emailData.find(
+        const emailObj = emailData.find(
             (email) => email.primary === true && email.verified === true
         );
+
+        const existingUser = await User.findOne({ email: emailObj.email });
+        if (existingUser) {
+            req.session.loggedIn = true;
+            req.session.user = existingUser;
+            return res.redirect('/');
+        } else {
+            const user = await User.create({
+                name: userData.name ? userData.name : 'Unknown',
+                username: userData.login,
+                email: emailObj.email,
+                password: '',
+                socialOnly: true,
+                location: userData.location
+            });
+            req.session.loggedIn = true;
+            req.session.user = user;
+            return res.redirect('/');
+        }
     } else {
         return res.redirect('/login');
     }
