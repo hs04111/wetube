1e3de7634d9a7d44a6c32f6bfb32cbe6a63e6d29
Seokyoung Han <hs04111@naver.com>
2021-05-27 12:52:38 +0900

    volume input

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -6,6 +6,9 @@ const volumeRange = document.getElementById('volume');
 
 // 대부분의 비디오 컨트롤은 HTML의 video element 부분을 참조하여 작성한다. 이는 MediaElement를 상속한 것이다. https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
 
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
 const handlePlayClick = (e) => {
     if (video.paused) {
         // video.paused는 video element가 멈춰있으면 true를 return한다.
@@ -24,10 +27,23 @@ const handleMute = (e) => {
         video.muted = true;
     }
 
-    volumeRange.value = video.muted ? 0 : 0.5;
+    volumeRange.value = video.muted ? 0 : volumeValue;
     muteBtn.innerText = video.muted ? 'Unmute' : 'Mute';
+};
+
+const handleVolumeChange = (event) => {
+    const {
+        target: { value }
+    } = event;
+    if (video.muted) {
+        video.muted = false;
+        muteBtn.innerText = 'Mute';
+    }
+    volumeValue = value;
+    video.volume = value;
 };
 
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
+volumeRange.addEventListener('input', handleVolumeChange);
 // video.addEventListener('play', handlePlay);  play와 pause에 대하여, video elemet의 event들을 참조할 것. https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
